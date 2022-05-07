5e339fb5cdef45fa79f71168139dc6b63ef6cade
monirulislamkhan <mr.monirulkhan@gmail.com>
2022-05-07 14:28:28 +0530

    react first commit by monirul

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import ItemList from './components/ItemList';
+import './App.css';
+function App() {
+  return (
+    <>
+      <div className="h2"></div>
+      <ItemList/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ItemList.js
@@ -0,0 +1,16 @@
+import './ItemList.scss';
+const ItemList = () => {
+  return (
+    <>
+      <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Test 1</h2>
+        <div className="expense-item__price">Rs.294.67</div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default ItemList;
